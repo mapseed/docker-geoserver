e9b9a31d9d3cfa943b1d2469e21cb227f5c24cd5
Luke Swart <lukeswart@gmail.com>
2015-02-14 15:55:02 +0000

    Fix run script

--- run.sh
+++ run.sh
@@ -4,18 +4,19 @@ docker rm geoserver
 docker kill geoserver-postgis
 docker rm geoserver-postgis
 
-DATA_DIR=~/geoserver_data
-if [ ! -d $DATA_DIR ]
-then
-    mkdir -p $DATA_DIR
-fi 
+#DATA_DIR=~/geoserver_data
+#if [ ! -d $DATA_DIR ]
+#then
+#    mkdir -p $DATA_DIR
+#fi
 
 docker run --name="geoserver-postgis" -t -d kartoza/postgis
 
 docker run \
-	--name=geoserver \
-	--link geoserver-postgis:postgis \
-        -v $DATA_DIR:/opt/geoserver/data_dir \
-	-p 8080:8080 \
-	-d \
-	-t lukeswart/geoserver
+  --name=geoserver \
+  --link geoserver-postgis:postgis \
+  -v /home/lucas/geoserver_data:/opt/geoserver/data_dir \
+  -v /home/lucas/docker-geoserver/resources/conf/web.xml:/usr/local/tomcat/conf/web.xml \
+  -p 8080:8080 \
+  -d \
+  -t lukeswart/geoserver
