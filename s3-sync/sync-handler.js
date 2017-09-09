1401c604359eef0e1c0e7937cb3157892d2d3bcd
Luke Swart <luke@lukeswart.net>
2017-09-09 00:50:11 -0700

    feat(syncHandler) implement stdout/stderr on sync exec

--- s3-sync/sync-handler.js
+++ s3-sync/sync-handler.js
@@ -1,6 +1,9 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec)
+
 // You can call this with the command:
 // curl --user 'user:pass' http://localhost:3000
-const handleSync = (req, res, next) => {
+const handleSync = async (req, res, next) => {
   const
     header = req.headers['authorization'] || '',        // get the header
     token = header.split(/\s+/).pop() || '',            // and the encoded auth token
@@ -12,17 +15,28 @@ const handleSync = (req, res, next) => {
   if (process.env.USERNAME === username &&
       process.env.PASSWORD === password) {
     console.log('success!!!')
-    res.writeHead(200, { 'Content-Type':'text/plain' })
-    // TODO: sync the ~/geoserver_data/[^data] to our geoserver-config bucket,
+    // sync the ~/geoserver_data/[^data] to our geoserver-config bucket,
     // and the ~/geoserver_data/data to our public gis bucket, eg:
-    // exec(`aws s3 sync --acl public-read ~/geoserver_data/data/ s3://geoserver-data.mapseed.org`, () => {
-    //   console.log("done syncing!")
-    // });
-    // TODO: send the output in our response! Possibly streaming it as well...
+
+    // TODO: pass in {encoding: 'buffer'} to make stdout a buffer!
+    //     const {err, stdout, stderr} = await exec('aws s3 sync ./geoserver-test/ s3:geoserver-data.mapseed.org/' )
+    const {err, stdout, stderr} = {
+      err: null,
+      stdout: 'this is stdout',
+      stderr: 'this is stderr',
+    }
+    if (err) {
+      res.writeHead(500, { 'Content-Type':'text/plain' })
+      res.end(`err: ${err}`)
+    } else {
+      res.writeHead(200, { 'Content-Type':'text/plain' })
+      // TODO: make these buffers!
+      res.end(`stdout: ${stdout}, stderr: ${stderr}`)
+    }
   } else {
     res.writeHead(401, { 'Content-Type':'text/plain' })
+    res.end(`invalid username:"${username}" and password: "${password}"`)
   }
-  res.end(`invalid username:"${username}" and password: "${password}"`)
   next()
 }
 
